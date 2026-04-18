581923b1b1d4ce314affc0dba880fc52786df3e3
Xuân Nhân Lê <lexuannhan2206@gmail.com>
2026-04-18 07:01:21 +0000

    photo-app-v1

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -8,41 +8,41 @@ import "./styles.css";
 /**
  * Define TopBar, a React component of Project 4.
  */
-function TopBar () {
-    const location = useLocation();
-    const params = useParams();
+function TopBar() {
+  const location = useLocation();
+  const params = useParams();
 
-    const getContextText = () => {
-      const userId = params.userId || location.pathname.split("/")[2];
-      const user = userId ? models.userModel(userId) : null;
+  const getContextText = () => {
+    const userId = params.userId || location.pathname.split("/")[2];
+    const user = userId ? models.userModel(userId) : null;
 
-      if (!user) {
-        return "Photo Sharing App";
-      }
-
-      if (location.pathname.startsWith("/photos/")) {
-        return `Photos of ${user.first_name} ${user.last_name}`;
-      }
-
-      if (location.pathname.startsWith("/users/")) {
-        return `${user.first_name} ${user.last_name}`;
-      }
-
+    if (!user) {
       return "Photo Sharing App";
-    };
+    }
 
-    return (
-      <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
-            Photo Sharing App
-          </Typography>
-          <Typography variant="h6" color="inherit">
-            {getContextText()}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
+    if (location.pathname.startsWith("/photos/")) {
+      return `Photos of ${user.first_name} ${user.last_name}`;
+    }
+
+    if (location.pathname.startsWith("/users/")) {
+      return `${user.first_name} ${user.last_name}`;
+    }
+
+    return "Photo Sharing App";
+  };
+
+  return (
+    <AppBar className="topbar-appBar" position="absolute">
+      <Toolbar>
+        <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
+          Lê Xuân Nhân - B23DCKH083
+        </Typography>
+        <Typography variant="h6" color="inherit">
+          {getContextText()}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default TopBar;
